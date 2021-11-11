7a7251938a801558da5ae81b15a32b30609744e8
Debarchan Nayek <dnayek2002@gmail.com>
2021-11-11 11:24:13 +0530

    Line Graph

--- src/App.js
+++ src/App.js
@@ -6,55 +6,46 @@ function App() {
     <div className="container">
       <header className="App-header">
         <Chart
-          type="bar"
-          // width={500}
-          // height={500}
+          type="line"
+          width={500}
+          height={500}
           series={[
             {
               name: "C1",
-              data: [100, 150, 50, 75, 67, 500],
+              data: [28, 29, 33, 36, 32, 32, 33],
               color: "#0d25d6",
             },
             {
               name: "C2",
-              data: [90, 110, 60, 85, 79, 300],
+              data: [12, 11, 14, 18, 17, 13, 13],
               color: "#ff0000",
-            },
-            {
-              name: "C3",
-              data: [105, 65, 68, 76, 72, 189],
-              color: "#dd0",
-            },
-            {
-              name: "C4",
-              data: [81, 181, 183, 290, 285, 40],
-              color: "#ff00a0",
-            },
-            {
-              name: "C5",
-              data: [250, 82, 91, 82, 60, 185],
-              color: "#C85C5C",
-            },
-            {
-              name: "C6",
-              data: [199, 118, 160, 185, 179, 198],
-              color: "#34BE82",
             },
           ]}
           options={{
-            theme:{
-              mode:'dark'
+            xaxis: {
+              tickPlacement: "on",
+              categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
             },
-            xaxis:{
-              tickPlacement:'on',
-              categories:['Jan','Feb','Mar','Apr','May','Jun']
+            chart: {
+              stacked: true,
             },
-            chart:{
-              stacked: true
+            dataLabels: {
+              enabled: true,
             },
-            tooltip:{
-              followCursor: true
-            }
+            stroke: {
+              curve: "smooth",
+            },
+            tooltip: {
+              followCursor: true,
+            },
+
+            // legend: {
+            //   position: "top",
+            //   horizontalAlign: "right",
+            //   floating: true,
+            //   offsetY: -25,
+            //   offsetX: -5,
+            // },
           }}
         />
       </header>
